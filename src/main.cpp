b9b874f91fcb9ae2260422b119898313a9ecc287
NGH <judah2013@naver.com>
2024-04-02 18:03:42 +0900

    First I/O Event
    1. Change framebuffer
    2. Input keyboard

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,13 @@
 #include <spdlog/spdlog.h>
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+// 윈도우 프레임버퍼(창) 크기 변경 이벤트 콜백함수
+void OnFramebufferSizeChange(GLFWwindow * window, int width, int height);
+// 키보드 입력 이벤트 콜백
+void OnKeyEvent(GLFWwindow* window, int key, int scanCode, int action, int mods);
+// 랜더링 함수
+void Render();
 
 int main(int argc, const char** argv) 
 {
@@ -16,6 +24,10 @@ int main(int argc, const char** argv)
         return -1;
     }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
     // glfw 윈도우 생성, 실패하면 에러 출력후 종료
     SPDLOG_INFO("Create glfw window");
     auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME,
@@ -26,14 +38,59 @@ int main(int argc, const char** argv)
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
+
+    // glad를 활용한 OpenGL 함수 로딩
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        SPDLOG_INFO("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version : {}", (char*)glVersion);
+
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
 
     // glfw 루프 실행, 윈도우 close 버튼을 누르면 정상 종료
     SPDLOG_INFO("Start main loop");
     while (!glfwWindowShouldClose(window)) 
     {
+        Render();
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void OnFramebufferSizeChange(GLFWwindow *window, int width, int height)
+{
+    SPDLOG_INFO("framebuffer size changed : ({} x {})", width, height);
+    glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow *window, int key, int scanCode, int action, int mods)
+{
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key, scanCode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repeat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) 
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void Render()
+{
+    glClearColor(0.1f, 0.2f, 0.3f, 0.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
 }
